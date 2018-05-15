54a13a03fdd27db6c7652aa7f6260158bba85692
abshinde221 <ams9mb@virginia.edu>
2018-05-15 19:47:17 -0400

    error with import

--- App.js
+++ App.js
@@ -1,23 +1,26 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Container } from './styles/grid'
+import { Stack Navigator, } from 'react-navigation'; // used to navigate between screens
+import Login from './screens/Login/login'
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
+// export default class App extends React.Component {
+//   render() {
+//     return (
+//       <Container>
+//         <Text>Open up App.js to start working on your app!</Text>
+//         <Text>Changes you make will automatically reload.</Text>
+//         <Text>Shake your phone to open the developer menu.</Text>
+//       </Container>
+//     );
+//   }
+// }
+
+export default StackNavigator({
+  Login: {
+    screen: 
+    navigationOptions: {
+      title: 'Login'
+    }
   }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
 });
---
+++ screens/Login/login.js
@@ -0,0 +1,14 @@
+// login screen
+// 
+
+import React from 'react';
+import { Text } from 'react-native';
+import { Container } from '../../styles/grid';
+
+export default function Login(props) {
+	return (
+		<Container>
+			<Text> Login Screen </Text>
+		</Container>
+	);
+}
